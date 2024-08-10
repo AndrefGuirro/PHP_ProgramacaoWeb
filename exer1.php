1b550b1ee853d5c1a002e036f8ccc70e61e5cfcc
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2024-08-10 09:16:12 -0300

    exer 1
    atualização

--- exer1.php
+++ exer1.php
@@ -1,50 +1,33 @@
 <?php require("cabecalho.php"); ?>
 <h1>Exercício 1</h1>
-
-<form action="exer1.php" method="POST">
-
-<div class="row">
-    <div class="col">
-        <label for="hora_entrada">Hora de Entrada</label>
-        <input type="time" id="hora_entrada"
-            name="hora_entrada" class="form-control" />
+<form action="exer1.php" method="post">
+    <div class="row">
+        <div class="col">
+            <label for="hora_inicial" class="form-label">
+                Informe a hora inicial:
+            </label>
+            <input type="time" name="hora_inicial" id="hora_inicial" class="form-control"/>
+        </div>
+        <div class="col">
+            <label for="hora_final" class="form-label">
+                Informe a hora final:
+            </label>
+            <input type="time" name="hora_final" id="hora_final" class="form-control"/>
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <label for="hora_saida">Hora de Saída</label>
-        <input type="time" id="hora_saida"
-            name="hora_saida" class="form-control" />
+    <div class="row">
+        <div class="col">
+            <button type="submit" class="btn btn-danger">Calcular</button>
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <button type="submit" class="btn btn-success">Calcular></button>
-    </div>
-</div>
-
 </form>
 
-
-<?php
-
-    function calcularTempo($hora_entrada, $hora_saida){
-        $hora_entrada = new DateTime($hora_entrada);
-        $hora_saida = new DateTime($hora_saida);
-        $resultado = $hora_saida->diff($hora_entrada);
-        $resultado = $resultado->format("%h:%s");
-        return $resultado;
+<?php 
+    if ($_POST){
+        $hora_incial = new DateTime($_POST['hora_inicial']);
+        $hora_final = new DateTime($_POST['hora_final']);
+        $diferenca = $hora_final->diff($hora_incial);
+        echo "Horas trabalhadas: ".$diferenca->format("%h:%I");
     }
 
-    if ($POST){
-        $hora_entrada = $_POST['$hora_entrada'];
-        $hora_saida = $_POST['$hora_saida'];
-        echo calcularTempo($hora_entrada, $hora_saida); 
-     }
-
-
-
-
-require("rodape"); ?>
+    require("rodape.php"); ?>
